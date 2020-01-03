603de9739922c59f0907775788be6691b4efe145
Tran Duc Quy <tranducquylhp@gmail.com>
2020-01-03 13:43:09 +0700

    add authentication service

---
+++ src/app/service/authentication.service.ts
@@ -0,0 +1,38 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {UserToken} from '../interface/user-token';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  private currentUserSubject: BehaviorSubject<UserToken>;
+  public currentUser: Observable<UserToken>;
+  update = new EventEmitter<string>();
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<UserToken>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): UserToken {
+    return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string) {
+    return this.http.post<any>('http://rate-project.herokuapp.com/login', {username, password})
+      .pipe(map(user => {
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        this.currentUserSubject.next(user);
+        this.update.emit('login');
+        return user;
+      }));
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+}
